97f72145019b7f71f76dc57f6cda63b8569d2ab4
OscarGlo <gloaguen.oscar@gmail.com>
2025-01-17 19:00:55 +0100

    🚧 Manysweeper proxy

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const https = require("https");
+const proxy = require('express-http-proxy');
 const fs = require("fs");
 const express = require("express");
 
 const app = express();
+
+app.use((req, res, next) => {
+    let redirect = false;
+
+    const path = req.path.split("/").slice(1);
+    if (path[0] === "manysweeper") {
+        path.splice(0, 1);
+        redirect = true;
+    }
+
+    if (redirect || req.subdomains[0] === "manysweeper")
+        return proxy(`https://localhost:8443/${path}`)(req, res, next);
+
+    next();
+});
 
 app.use(express.static("public"))
 
@@ -13,4 +29,4 @@ const httpsServer = https.createServer({
     cert: fs.readFileSync('ssl/domain.cert.pem', 'utf8')
 }, app);
 
-httpsServer.listen(8443);
+httpsServer.listen({ port: 443 }, () => console.log("Listening on https://localhost:443/"));
