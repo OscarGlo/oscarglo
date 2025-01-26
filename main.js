f6d5d36bee537ef928b2a27a0351a0c6b5c0c09e
OscarGlo <gloaguen.oscar@gmail.com>
2025-01-26 22:01:55 +0100

    🚧 WebSocket "Invalid frame header"

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
+const httpsServer = https.createServer({
+    key: fs.readFileSync('ssl/private.key.pem', 'utf8'),
+    cert: fs.readFileSync('ssl/domain.cert.pem', 'utf8')
+}, app);
 const proxy = createProxyServer({ target: 'https://localhost:8443', ws: true });
+
+httpsServer.on("upgrade", (req, socket, head) => {
+    console.log("UPGRADE")
+    proxy.ws(req, socket, head);
+})
 
 app.use((req, res, next) => {
     if (req.subdomains[0] === "manysweeper") {
@@ -16,13 +25,14 @@ app.use((req, res, next) => {
     next();
 });
 
+server.on("upgrade", (req, socket, head) => {
+    if (req.subdomains[0] === "manysweeper") {
+        proxy.ws(req, socket, head);
+    }
+});
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => res.sendFile("public/index.html"));
-
-const httpsServer = https.createServer({
-    key: fs.readFileSync('ssl/private.key.pem', 'utf8'),
-    cert: fs.readFileSync('ssl/domain.cert.pem', 'utf8')
-}, app);
 
 httpsServer.listen({ port: 443 }, () => console.log("Listening on https://localhost:443/"));
