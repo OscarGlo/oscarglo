483b5305ede7c8dce561f99fdb8b0b86b169472a
OscarGlo <gloaguen.oscar@gmail.com>
2025-08-13 23:47:57 +0200

    🐛 Proxy redirect + upgrade

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ const httpsServer = https.createServer({
     cert: fs.readFileSync('ssl/domain.cert.pem', 'utf8')
 }, app);
 
-const manysweeperProxy = createProxyServer({ target: 'https://oscarglo.dev:9000', ws: true });
-const canvasProxy = createProxyServer({ target: 'https://oscarglo.dev:9001' });
+const manysweeperProxy = createProxyServer({ target: 'http://oscarglo.dev:9000', ws: true });
+const canvasProxy = createProxyServer({ target: 'http://oscarglo.dev:9001' });
 
-httpsServer.on("upgrade", (req, socket, head) => proxy.ws(req, socket, head));
+httpsServer.on("upgrade", (req, socket, head) => manysweeperProxy.ws(req, socket, head));
 
 app.use((req, res, next) => {
     if (req.subdomains[0] === "manysweeper")
