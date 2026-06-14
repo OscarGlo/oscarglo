1d3deebd7b1a722beae7cec3a99f9ec23e72a6c3
OscarGlo <gloaguen.oscar@gmail.com>
2026-06-14 10:47:12 +0200

    🐛 Catch proxy errors

--- main.js
+++ main.js
@@ -34,10 +34,15 @@ const canvasProxy = createProxyServer({target: 'http://oscarglo.dev:9001'});
 httpsServer.on("upgrade", (req, socket, head) => manysweeperProxy.ws(req, socket, head));
 
 app.use((req, res, next) => {
-    if (req.subdomains[0] === "manysweeper")
-        return manysweeperProxy.web(req, res);
-    if (req.subdomains[0] === "canvas")
-        return canvasProxy.web(req, res);
+    try {
+        if (req.subdomains[0] === "manysweeper")
+            return manysweeperProxy.web(req, res);
+        if (req.subdomains[0] === "canvas")
+            return canvasProxy.web(req, res);
+    } catch (e) {
+        console.error(`[${new Date().toISOString().replace("T", " ")}]`, req.url, e);
+        return res.sendStatus(500);
+    }
 
     next();
 });
@@ -47,5 +52,5 @@ app.use(express.static("public"));
 app.get("/", (req, res) => res.sendFile("public/index.html"));
 
 httpsServer.listen({port: 443}, () => {
-    console.log(`[${new Date().toUTCString()}] Listening on https://localhost:443/`);
+    console.log(`[${new Date().toISOString().replace("T", " ")}] Listening on https://localhost:443/`);
 });
